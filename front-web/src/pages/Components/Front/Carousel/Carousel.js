2c1fa954e2cc60b55dea01baff28cc59a11ea077
fadi dabboura <fadidab1998@gmail.com>
2024-01-18 10:22:26 +0100

    Initial Updated commit

--- front-web/src/pages/Components/Front/Carousel/Carousel.js
+++ front-web/src/pages/Components/Front/Carousel/Carousel.js
@@ -9,31 +9,45 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Carousel =() => {
     const images = ['/01.jpg', '/02.jpg', '/03.jpg', '/02.jpg', '/03.jpg']; // Replace with your image URLs
-
+    const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+    const getSlideSize = (index) => {
+      // Adjust the size based on the active state or any other condition
+      return index === activeSlideIndex ? ' min-w-[250px]  z-0 max-h-[375px] ' : ' max-w-[170px] max-h-[255px]';
+    };
     
     const handleSlideClick = (index) => {
         // Navigate to the clicked slide
         if (swiper) {
           swiper.slideTo(index); // Use the slideTo method to navigate
+          
         }
       };
       let swiper;
 
     const swiperParams = {
-       
-        pagination: {
+      spaceBetween:4, // Adjust the space between slides as needed
+      slidesPerView: "auto", // Adjust the number of slides per view based on your design
+      simulateTouch: true,
+      clickable:true,
+      pagination: {
           el: '.swiper-pagination',
           clickable: true,
+          
         },
-        slidesPerView: 'auto',
-        onSwiper: (swiperInstance) => {
+        
+      onSwiper: (swiperInstance) => {
             // Save Swiper instance to a variable
             swiper = swiperInstance;
           },
           initialSlide:2, 
+      onSlideChange:(swiper) => {
+        
+            setActiveSlideIndex(swiper.activeIndex);
+          }
       };
   return (
     <Swiper
@@ -41,48 +55,43 @@ const Carousel =() => {
 
       className='text-white w-full h-full  mySwiper bg-transparent	mt-[-5rem]'
       
-      centeredSlides={true}
+      centeredSlides= {true}
       freeMode={true}
       breakpoints={{
-        350: {
-            slidesPerView: 2,
-            spaceBetween: 1,
-          },
+         350: {
+          slidesPerView:2,
+          centeredSlides: true,
+          spaceBetween:20
+        },
         640: {
           slidesPerView: 3,
-          spaceBetween:1,
+          centeredSlides: true,
+          spaceBetween:20
         },
         768: {
-          slidesPerView: 3,
-          spaceBetween:3,
-        },
-        900: {
           slidesPerView: 4,
-          spaceBetween:1,
+          centeredSlides: true,
+          spaceBetween:20
         },
         1024: {
-          slidesPerView: 4,
-          spaceBetween:1,
-        },
-        1200: {
           slidesPerView: 5,
-          spaceBetween:1,
         },
       }}
       {...swiperParams}
     >
      {images.map((image, index) =>{
         return (
-        <SwiperSlide key={index} className='w-[250px] max-sm:w-[200px] sm:w-[200px] md:w-[200px] lg:w-[250px] xl:w-[250px] z-0 max-sm:h-[300px] sm:h-[300px] md:w-[300px] lg:h-[375px] xl:h-[375px] h-[375px] drop-shadow-none shadow-none z-0 relative cursor-pointer ' onClick={()=>handleSlideClick(index)} >{({ isActive }) => (
+        <SwiperSlide key={index} className={` ${getSlideSize(index)} mx-auto p-0 drop-shadow-none shadow-none z-0 relative cursor-pointer `}  onClick={()=>handleSlideClick(index)}>{({ isActive }) => (
            <>
-           <div className='absolute top-0 left-0 right-0 bottom-0 '>
-            <div className='w-[250px] max-sm:w-[200px] sm:w-[200px] md:w-[200px] lg:w-[250px] xl:w-[250px] z-0 max-sm:h-[300px] sm:h-[300px] md:h-[300px] lg:h-[375px] xl:h-[375px] h-[375px] relative mx-auto 	drop-shadow-none shadow-none '>
-            <Image src={image}  className={`transition ease-in-out delay-200 ${isActive?"p-0 ease-linear ":"px-3 py-6 pb-3 ease-linear "}`} fill priority objectFit='fill' alt={`Slide ${index + 1}`} />
+           <div className={`min-w-full min-h-full absolute top-0 left-0 right-0 bottom-0 p-0 m-0 `} >
+            <div className={`  relative drop-shadow-none shadow-none  ${isActive?" min-w-full  z-0 min-h-full ":"  w-[170px] h-[255px] mt-12"}`} >
+            <Image src={image}  className={`transition  ease-in-out delay-200  `} fill priority objectFit='cover' alt={`Slide ${index + 1}`} />
             
             
-            <div className={`absolute  left-0 right-0   top-full bg-transparent w-full h-full z-10 ${isActive?"p-0 ":"mt-[-2.5rem] px-4 py-9"}`}>
-            <div className='relative w-full h-full z-10'>
-            <Image src={image} className='rotate-180 opacity-20 z-10 '  fill priority objectFit='content' alt={`Slide ${index + 1}`} />
+            <div className={`absolute  left-0 right-0   top-full bg-transparent w-full h-full z-10 mx-auto ${isActive?"":"  "}`}>
+            <div className='min-w-full min-h-full relative mx-auto 	drop-shadow-none shadow-none '>
+            <Image src={image}  className={`rotate-180 opacity-20 z-10  transition ease-in-out delay-200 ${isActive?"p-0 ease-linear ":""}`} fill priority objectFit='fill' alt={`Slide ${index + 1}`} />
+
             <div className='absolute top-0 left-0 right-0 text-center p-2 w-full z-50'>
                 <Link className='z-50' href="/">
                 Wedding
--- front-web/src/pages/index.js
+++ front-web/src/pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
   return (
    <Layout>
   
-    <div className='max-sm:w-10/12 sm:w-full md:w-full lg:w-full xl:w-[80%]  mx-auto max-sm:h-[28.6rem] sm:h-[30rem] lg:[35rem] xl:h-[40rem] h-[40rem] mt-[8rem]'>
-    <Carousel className="text-white"/>
+    <div className='max-sm:w-full sm:w-full md:w-full lg:w-[70%] xl:w-[70%] 2xl:w-[70%]  mx-auto max-sm:h-[28.6rem] sm:h-[30rem] lg:[35rem] xl:h-[40rem] h-[40rem] mt-[8rem]'>
+    <Carousel className="text-white "/>
     </div>
    </Layout>
   )
