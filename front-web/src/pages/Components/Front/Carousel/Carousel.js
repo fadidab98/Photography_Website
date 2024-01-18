985045f16ca8bc9ff2442aac920c7143163ef062
fadi dabboura <fadidab1998@gmail.com>
2024-01-18 11:11:33 +0100

    Initial Updated commit

--- front-web/src/pages/Components/Front/Carousel/Carousel.js
+++ front-web/src/pages/Components/Front/Carousel/Carousel.js
@@ -9,25 +9,31 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Carousel =() => {
+     const swiperRef = useRef(2);
+
     const images = ['/01.jpg', '/02.jpg', '/03.jpg', '/02.jpg', '/03.jpg']; // Replace with your image URLs
     const [activeSlideIndex, setActiveSlideIndex] = useState(0);
     const getSlideSize = (index) => {
       // Adjust the size based on the active state or any other condition
       return index === activeSlideIndex ? ' min-w-[250px]  z-0 max-h-[375px] ' : ' max-w-[170px] max-h-[255px]';
     };
-    
     const handleSlideClick = (index) => {
-        // Navigate to the clicked slide
-        if (swiper) {
-          swiper.slideTo(index); // Use the slideTo method to navigate
-          
-        }
-      };
-      let swiper;
+      console.log("clicked",)
 
+      swiperRef.current.slideTo(index); // Use the slideTo method to navigate
+          
+      
+      };
+   
+     
+      const handleSlideChange=(swiper) => {
+        console.log(swiper.activeIndex)
+        setActiveSlideIndex(swiper.activeIndex);
+
+      }
     const swiperParams = {
       spaceBetween:4, // Adjust the space between slides as needed
       slidesPerView: "auto", // Adjust the number of slides per view based on your design
@@ -41,20 +47,17 @@ const Carousel =() => {
         
       onSwiper: (swiperInstance) => {
             // Save Swiper instance to a variable
-            swiper = swiperInstance;
+            swiperRef.current = swiperInstance;
           },
           initialSlide:2, 
-      onSlideChange:(swiper) => {
-        
-            setActiveSlideIndex(swiper.activeIndex);
-          }
+      onSlideChange:handleSlideChange
       };
   return (
     <Swiper
       // install Swiper modules
 
       className='text-white w-full h-full  mySwiper bg-transparent	mt-[-5rem]'
-      
+      ref={swiperRef}
       centeredSlides= {true}
       freeMode={true}
       breakpoints={{
@@ -81,7 +84,10 @@ const Carousel =() => {
     >
      {images.map((image, index) =>{
         return (
-        <SwiperSlide key={index} className={` ${getSlideSize(index)} mx-auto p-0 drop-shadow-none shadow-none z-0 relative cursor-pointer `}  onClick={()=>handleSlideClick(index)}>{({ isActive }) => (
+        <SwiperSlide
+         key={index}
+        className={` ${getSlideSize(index)} mx-auto p-0 drop-shadow-none shadow-none z-0 relative cursor-pointer `} 
+        onClick={()=>handleSlideClick(index)}>{({ isActive }) => (
            <>
            <div className={`min-w-full min-h-full absolute top-0 left-0 right-0 bottom-0 p-0 m-0 `} >
             <div className={`  relative drop-shadow-none shadow-none  ${isActive?" min-w-full  z-0 min-h-full ":"  w-[170px] h-[255px] mt-12"}`} >
